b73406d01c7b85ec5ef1d82eeecaefc6686f0d14
Phillip Lam <plam89@ucla.edu>
2021-07-01 00:11:38 -0700

    second and minute hands move incrementally, hour hand still does not

--- main.js
+++ main.js
@@ -6,40 +6,38 @@ const minuteHand = document.querySelector('.minute-hand')
 const hourHand = document.querySelector('.hour-hand')
 const digitalCurrentTime = document.querySelector('h3')
 
-let seconds;
 
 function getCurrentTime(date) {
   let hours = date.getHours();
   let minutes = date.getMinutes();
-  seconds = Number(date.getSeconds())
+  let currentSeconds = new Date;
+  seconds = Number(currentSeconds.getSeconds())
+  secondsHand.style.transform = `rotate(${seconds*6}deg)`
+  const ampm = hours >= 12 ? 'P.M.' : 'A.M.';
 
-  const ampm = hours >= 12 ? 'pm' : 'am';
   hours = hours % 12;
   hours = hours ? hours : 12;
+  hours = Number(hours)
+
   hourHand.style.transform = `rotate(${hours*30}deg)`
   minuteHand.style.transform = `rotate(${minutes*6}deg)`
   minutes = minutes < 10 ? '0'+minutes : minutes;
 
 
-  const strTime = hours + ':' + minutes + ' ' + ampm;
+  const strTime = hours + ':' + minutes + ' ' + ': ' + seconds + ' ' + ampm;
+
+  // if (minutes >= 59 && seconds >= 59) {
+  //   hourHand.style.transform = `rotate(${hours*6}deg)`
+  // }
+
   return strTime;
 }
-console.log(getCurrentTime(new Date));
-console.log('current seconds', seconds)
-
-
+// console.log(getCurrentTime(new Date));
+// console.log('current seconds', seconds)
 
 
 
 setInterval(function() {
   digitalCurrentTime.textContent = `Current Time: ${getCurrentTime(new Date)}`
-  let currentSeconds = new Date
-  seconds = currentSeconds.getSeconds()
-  secondsHand.style.transform = `rotate(${seconds*6}deg)`
-
-  if (seconds >= 59) {
-      let currentHour = (Number(getCurrentTime(new Date).split('').slice(0,2).join(',').replace(',', '')))
-      hourHand.style.transform = `rotate(${currentHour*30}deg)`
-  }
-
-},10)
+  console.log(digitalCurrentTime.textContent.slice(17,19))
+},1000)
