efbf58042691367f11d7dbadc0e80071e3496603
Phillip Lam <plam89@ucla.edu>
2021-06-30 20:57:32 -0700

    displaying numbers on clock properly using pseudo elements and JS

---
+++ main.js
@@ -0,0 +1,6 @@
+
+
+
+document.querySelector('span:nth-child(1)').textContent = ''
+document.querySelector('span:nth-child(2)').textContent = ''
+document.querySelector('span:nth-child(3)').textContent = ''
