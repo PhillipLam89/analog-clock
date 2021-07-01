1ea79f40471500782575833693f42639d40425bf
Phillip Lam <plam89@ucla.edu>
2021-06-30 21:43:59 -0700

    set up hr, min , seconds hands and refactored some JS

--- main.js
+++ main.js
@@ -1,15 +1,6 @@
 
+const clockNumbers = document.querySelectorAll('span')
 
-
-document.querySelector('span:nth-child(1)').textContent = ''
-document.querySelector('span:nth-child(2)').textContent = ''
-document.querySelector('span:nth-child(3)').textContent = ''
-document.querySelector('span:nth-child(4)').textContent = ''
-document.querySelector('span:nth-child(5)').textContent = ''
-document.querySelector('span:nth-child(6)').textContent = ''
-document.querySelector('span:nth-child(7)').textContent = ''
-document.querySelector('span:nth-child(8)').textContent = ''
-document.querySelector('span:nth-child(9)').textContent = ''
-document.querySelector('span:nth-child(10)').textContent = ''
-document.querySelector('span:nth-child(11)').textContent = ''
-document.querySelector('span:nth-child(12)').textContent = ''
+for (let val of clockNumbers) {
+  val.textContent = ''
+}
